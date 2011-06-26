6854294bdf62573ce9d95ee9d56893b5d2c4e589
Don Page <goodies4uall@gmail.com>
2011-06-26 13:12:56 -0600

    Minor changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Fbapp::Application.initialize!
+
+# unset for hostmonster
+# ENV['GEM_PATH'] = '/home2/pagedonc/ruby/gems:/usr/lib64/ruby/gems/1.8'
+
